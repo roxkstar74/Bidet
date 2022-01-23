668a731f05f4ca2e08af805228d9c7020a4992c2
Rox Works <dsg6874@g.rit.edu>
2022-01-23 00:05:46 -0500

    no

--- src/getData.js
+++ src/getData.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-export const getData = async() => {
+const getData = async() => {
     console.log('getData');
     const backendURL = process.env.BACKEND_URL || 'http://localhost:5000';
     const response = await axios.get(`${backendURL}/`);
